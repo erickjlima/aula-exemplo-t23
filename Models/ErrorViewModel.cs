f6ba4bd39c8c6592404aeeef9250b2d971c24067
eric <ericjosedelima@gmail.com>
2022-11-26 19:28:39 -0300

    vmcatualizado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aula_exemplo_t23.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
